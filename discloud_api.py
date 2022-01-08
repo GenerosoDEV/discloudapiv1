94c9f353fcd800850b4966d62bca20c645add921
GenerosoDEV <flavioa.generoso@gmail.com>
2022-01-08 16:52:43 -0300

    50%

---
+++ discloud_api.py
@@ -0,0 +1,41 @@
+import requests
+
+class BotStatus():
+    def __init__(self, bot_id: int=None, api_token: str=None):
+        r = requests.get(f"https://discloud.app/status/bot/{bot_id}", headers={"api-token":api_token})
+        result = r.json()
+        headers = r.headers
+        self.bot_id = result['bot_id']
+        self.info = result['info']
+        self.container = result['container']
+        self.cpu = result['cpu']
+        self.memory = result['memory']
+        self.last_restart = result['last_restart']
+        self.ratelimit = headers['x-ratelimit-limit']
+        self.ratelimit_remaining = headers['x-ratelimit-remaining']
+        self.ratelimit_reset = headers['x-ratelimit-reset']
+
+class UserStatus():
+    def __init__(self, api_token: str=None):
+        r = requests.get("https://discloud.app/status/user", headers={"api-token":api_token})
+        result = r.json()
+        headers = r.headers
+        self.userID = result['userID']
+        self.plan = result['plan']
+        self.lastDataLeft = result['lastDataLeft']
+        self.planDataEnd = result['planDataEnd']
+        self.ratelimit = headers['x-ratelimit-limit']
+        self.ratelimit_remaining = headers['x-ratelimit-remaining']
+        self.ratelimit_reset = headers['x-ratelimit-reset']
+"""
+class BotRestart():
+    def __init__(self, bot_id: str=None, api_token: str=None):
+        r = requests.post(f"https://discloud.app/status/bot/{bot_id}/restart", headers={"api-token":api_token})
+        result = r.json()
+        print(result)
+        headers = r.headers
+
+        self.ratelimit = headers['x-ratelimit-limit']
+        self.ratelimit_remaining = headers['x-ratelimit-remaining']
+        self.ratelimit_reset = headers['x-ratelimit-reset']
+"""
